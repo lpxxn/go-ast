ecff0faff734cd520227d5eeefd957e21dac061d
lpxxn <mi_duo@live.com>
2023-12-28 18:13:19 +0800

    feat: ast

--- a1cli/m_test.go
+++ a1cli/m_test.go
@@ -17,6 +17,10 @@ func TestAST1(t *testing.T) {
 	for _, s := range f.Imports {
 		t.Log("import: ", s.Path.Value)
 	}
+	for _, decl := range f.Decls {
+		t.Logf("decl: %T detail:  %#v", decl, decl)
+	}
+
 	/*
 		但是结构体中最重要的其实是File.Decls成员，它包含了当前文件全部的包级声明信息（包含导入信息）。
 		即使没有File.Imports成员，我们也可以从File.Decls声明列表中获取全部导入包的信息。
